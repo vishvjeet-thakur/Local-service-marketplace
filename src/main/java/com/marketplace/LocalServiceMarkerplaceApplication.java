c1ca6b9f3bbc4c361d891fde98e9eaf8fcaaa238
vishvjeet-thakur <vishwjeetthakur995@gmail.com>
2024-09-28 07:56:00 +0530

    main added

--- src/main/java/com/marketplace/LocalServiceMarkerplaceApplication.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.marketplace;
-
-public class LocalServiceMarkerplaceApplication {
-    
-}
---
+++ src/main/java/com/marketplace/LocalServiceMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalServiceMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalServiceMarketplaceApplication.class, args);
+    }
+
+}
